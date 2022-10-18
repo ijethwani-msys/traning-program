c4bae82fae04ecbb1e89ea9c8d5e1f293a628b9b
ijethwani-msys <ijethwani@msystechnologies.com>
2022-10-18 12:02:15 +0530

    added

--- assignment.py
+++ assignment.py
@@ -5,18 +5,11 @@ class Asignment(Institute):
     def group_creation_with_trainer(self,technology_name):
         tranee_list = self.get_trainee_by_technology(technology_name=technology_name)
         trainer_list = self.get_trainer_by_technology(technology_name=technology_name)
-        lenght_of_trainer_list = len(trainer_list)
-        lenght_of_trainee_list = len(tranee_list)
-        remaining_trainee= lenght_of_trainee_list%lenght_of_trainer_list
-        # data_dict = {
-        #     "trainer_id":self.id
-        #     "trainee_list":[
-
-                
-        #     ]
-
-
-        # }
+        for trainer in trainer_list:
+            data_dict = {
+                "trainer_id":1992,
+                "trainee_list":[23414,23414,23414,23414]
+            }
 
 
 obj = Asignment()
@@ -24,6 +17,14 @@ obj.group_creation_with_trainer("Python")
 
 
         
+# 1 list - 2
+# 1 = 1 3 5 7 9 11
+# 2 = 2 4 6 8 10 12
+
+# 1 = 1 4 7 10
+# 2 = 2 5 8 11
+# 3 = 3 6 9 12
+
 
 
 
